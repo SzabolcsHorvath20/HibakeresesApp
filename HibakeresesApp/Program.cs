dfc87bf54cb13c483d290ccfe0e3f6f26c9b0511
Garrus20 <garruswasab@gmail.com>
2020-02-28 08:28:03 +0100

    Add project files.

---
+++ HibakeresesApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HibakeresesApp
+{
+    class Program
+    {
+        static int MaxKeres(List<int> l)
+        {
+            int maxIndex = 0;
+            for (int i = 1; i < l.Count; i++)
+            {
+                Debug.WriteLine(maxIndex + " " + l[maxIndex], "valtozok");
+                if (l[i] > l[maxIndex])
+                {
+                    maxIndex = 1;
+                }
+            }
+            return l[maxIndex];
+        }
+
+        static void Main(string[] args)
+        {
+            var lista = new List<int>();
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                lista.Add(r.Next());
+            }
+            Console.WriteLine(MaxKeres(lista));
+            Console.ReadKey();
+        }
+    }
+}
